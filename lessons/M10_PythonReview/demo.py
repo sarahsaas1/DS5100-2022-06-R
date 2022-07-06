d6d9a09e2769575d5491c89483840afadabad437
Rafael Alvarado <ontoligent@gmail.com>
2022-07-05 23:24:03 -0400

    Updates for M11

--- lessons/M10_PythonReview/demo.py
+++ lessons/M10_PythonReview/demo.py
@@ -1,11 +1,14 @@
 # VS Code Demo File
 
-# Import Pandas and Numpy
-import numpy as np
+#%% 
 import pandas as pd
+import numpy as np
 
-# Create a simple DataFrame
+
+#%% Create a simple DataFrame
 a = np.random.random(10)
 b = np.random.random(10)
 df = pd.DataFrame(dict(a=a, b=b))
 df.index.name = 'obs_id'
+
+# %%
